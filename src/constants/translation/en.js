cefc5f99df1f8e51378c4c891c2ca18669c20679
luis juca <lhjuca@gmail.com>
2023-10-10 19:11:15 -0300

    feat: add translation file

---
+++ src/constants/translation/en.js
@@ -0,0 +1,8 @@
+export const translation ={
+        COPYRIGHT :"Copyright",
+        PRIVACY_POLICY:"Privacy Policy",
+        PROFILE:"Profile",
+        LEAGUES:"Leagues",
+        LOGIN:"Login",
+        TERMS_OF_USE:"Terms of Use",
+}
