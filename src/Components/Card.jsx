aa2f44afab3840dcc3238686cdec88f1c4fa2b69
luis juca <lhjuca@gmail.com>
2023-10-11 00:46:57 -0300

    refactor:mocked card template, added card image placeholder

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export default function Card() {
+const  Card = () => {
+
   return (
-    <div className='flex flex-col border border-gray p-3 rounded-md max-w-md min-h-[200px]'>
-        Card
+    <div style={{ backgroundImage: "url('src/assets/images/track-placeholder.jpg')" }} className=' flex flex-col border border-gray p-3 rounded-md max-w-md min-h-[200px] justify-start bg-cover'>
+      <div className='backdrop-blur-md backdrop-grayscale-0 bg-white/30 p-2 rounded w-80 h-28'>
+        <p className='text-2xl'>card title</p>
+        <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate fuga incidunt!</p>
+      </div>
         {/* background image with info on the left or right */}
     </div>
   )
 }
+export default Card
