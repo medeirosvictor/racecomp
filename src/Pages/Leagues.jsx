04ade80661a32224d732deec730fc524dccad981
luis juca <lhjuca@gmail.com>
2023-10-11 19:58:53 -0300

    feat: implemented mocked create league Form

--- src/Pages/Leagues.jsx
+++ src/Pages/Leagues.jsx
@@ -3,46 +3,95 @@ import { Navigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
 import Form from '../Components/Form';
 import FormField from '../Components/FormField';
-import { leagueFormFields } from '../constants/leagueFormConstants';
+import { translation } from '../constants/translation/en';
+import { users } from '../constants/mocks/usersMock';
+import FormFieldSelect from '../Components/FormFieldSelect';
+import FormFieldOptions from '../Components/FormFieldOptions';
 
-const Leagues = () =>{
+
+
+const initialState = {
+  title:"",
+  startDate:"",
+  selectedUsers:[]
+}
+
+const Leagues = () => {
 
   const { user } = UserAuth();
-  const [titleValue,setTitleValue] = useState('')
-  const [testValue,setTestValue] = useState('')
+  const [leagueData,setLeagueData] = useState(initialState)
 
-  const handleSetTitleValue = useCallback( value => {
-      setTitleValue(value)
+
+  const handleSetTitleValue = useCallback( title => {
+    setLeagueData(prev =>({
+      ...prev,
+      title
+    }))
   },[])
 
-  const handleSetTestValue = useCallback( value => {
-    setTestValue(value)
+  const handleSetLeagueStart = useCallback( leagueStartDate => {
+    setLeagueData(prev =>({
+      ...prev,
+      startDate:leagueStartDate,
+    }))
   },[])
+
+  const handleSetSelectedUsers = useCallback((userId) => {
+    setLeagueData((prev) => {
+      if (prev.selectedUsers.includes(userId)) {
+        return {
+          ...prev,
+          selectedUsers: prev.selectedUsers.filter(id => id !== userId),
+        }
+      } else {
+        return {
+          ...prev,
+          selectedUsers:[...prev.selectedUsers, userId],
+        };
+      }
+    });
+  }, []);
 
   if (!user) {
       return <Navigate to="/" />
   }
 
   return (
-      <div  style={{ backgroundImage: "url('src/assets/images/league-form-bg.png')" }} className="flex justify-center content-center items-center bg-center w-full h-screen">
-        
-        <Form styles ={"backdrop-blur-md backdrop-grayscale-0 bg-white/30 p-2 shadow-md rounded"} title ={"League Creation Form"}>
-          <FormField 
-            label ={leagueFormFields.TITLE_FIELD.label} 
-            placeHolder ={leagueFormFields.TITLE_FIELD.placeHolder} 
-            value ={titleValue}
-            onChange={handleSetTitleValue}
-            key={leagueFormFields.label}
-          />
+      <div className="flex justify-center content-center items-center  w-screen h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+        <Form 
+          handleCancel ={()=>{ console.log("handleCancel was called")}}
+          handleSubmit ={()=>{ console.log("handleSubmit was called")}}
+          styles ={"backdrop-blur-md backdrop-grayscale-0 bg-white/30 p-2 shadow-md rounded"} title ={"League Creation Form"}
+        >
           <FormField
-            label ={leagueFormFields.VALUE_FIELD.label} 
-            placeHolder ={leagueFormFields.VALUE_FIELD.placeHolder}
-            value ={testValue}
-            onChange={handleSetTestValue}
-            key={leagueFormFields.label}
+            label = {translation.LEAGUE_FORM_FIELD_TITLE}
+            placeholder = {translation.LEAGUE_FORM_PLACEHOLDER}
+            value = {leagueData.title}
+            onChange={handleSetTitleValue}
+            key={translation.LEAGUE_FORM_TITLE}
           />
-          </Form>
-        </div>
+          <label className=" text-gray-700 text-sm font-bold mb-2 text-xl" htmlFor={"leagueStartDate"}>
+            {translation.START_DATE}:
+          </label>
+          <input type="date" className="rounded w-32" name="leagueStartDate" id="leagueStartDateInput" onChange={e => handleSetLeagueStart( e.target.value)} />
+          <FormFieldSelect placeholder = {translation.SELECT_PARTICIPANTS} optionLabel ={translation.SELECT_AN_USER} handleSelect ={handleSetSelectedUsers} values ={users}>
+            <FormFieldOptions options ={users}/>
+          </FormFieldSelect>
+          <div className ="h-18 w-full back">
+            <label  htmlFor={"selectedUsers"}  className="mt-2 text-xl  font-bold text-gray-700">
+              {translation.SELECTED_USERS}
+            </label>
+            <ul name="selectedUsers" className=" m-auto my-4 w-48 h-24 overflow-y-auto bg-white rounded">
+              {
+                users?.filter(userData => leagueData.selectedUsers?.includes(userData?.uid)).map(item =>{
+                  return (<li key={item.uid}>{item?.displayName}</li>)
+                })
+              }
+            </ul>
+            </div>
+        
+        </Form>
+      </div>
     )
 }
 
