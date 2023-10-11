10b130b5d9dd62e326120254ee99c7a6d1856b92
luis juca <lhjuca@gmail.com>
2023-10-11 19:58:21 -0300

    feat: created users mock

---
+++ src/constants/mocks/usersMock.js
@@ -0,0 +1,32 @@
+export const users = [
+    {
+      uid: 'user1',
+      displayName: 'John Doe',
+      email: 'john@example.com',
+    },
+    {
+      uid: 'user2',
+      displayName: 'Jane Smith',
+      email: 'jane@example.com',
+    },
+    {
+      uid: 'user3',
+      displayName: 'Bob Johnson',
+      email: 'bob@example.com',
+    },
+    {
+      uid: 'user4',
+      displayName: 'Mr. Smith',
+      email: 'jane@example.com',
+    },
+    {
+      uid: 'user5',
+      displayName: 'Ben Testinson',
+      email: 'ben@example.com',
+    },
+    {
+      uid: 'user6',
+      displayName: 'Gwen Testinson',
+      email: 'gwen@example.com',
+    },
+  ];
