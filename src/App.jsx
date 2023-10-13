8bedd2214b4b90a351dfc600003f6a91df973a4c
Vinicius-Medeiros <viniciusaomedeiros@gmail.com>
2023-10-13 07:53:31 -0300

    first commit of Card Preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,55 @@
-import './App.css'
-import Home from './Pages/Home'
-import Leagues from './Pages/Leagues'
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import './App.css';
+import Home from './Pages/Home';
+import Leagues from './Pages/Leagues';
+import Profile from './Pages/Profile';
+import Root from './Root';
 
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import Root from './Root'
-import { AuthContextProvider } from './context/AuthContext';
-import Profile from './Pages/Profile';
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from 'react-router-dom';
 import ProtectedRoute from './Components/ProtectedRoute';
-import Login from './Pages/Login.jsx';
+import { AuthContextProvider } from './context/AuthContext';
 import { AboutUs } from './Pages/AboutUs';
 import { ContactUs } from './Pages/ContactUs';
+import Login from './Pages/Login.jsx';
+import { Page404 } from './Pages/Page404';
 import { PrivacyPolicy } from './Pages/PrivacyPolicy';
 import { TermsOfService } from './Pages/TermsOfService';
-import { Page404 } from './Pages/Page404';
-
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route index element={<Home />} />
-      <Route path="leagues" element={<Leagues />} />
-      <Route path="login" element={<Login />} />
-      <Route path="profile" element={ <ProtectedRoute><Profile /></ProtectedRoute>} />
-      <Route path='contact-us' element={<ContactUs />} />
-      <Route path='about-us' element={<AboutUs />} />
-      <Route path='privacy-policy' element={<PrivacyPolicy />} />
-      <Route path='terms-of-service' element={<TermsOfService />} />
-      <Route path="*" element={<Page404 />} />
-    </Route>
-  )
-)
+    createRoutesFromElements(
+        <Route path="/" element={<Root />}>
+            <Route index element={<Home />} />
+            <Route path="leagues" element={<Leagues />} />
+            <Route path="login" element={<Login />} />
+            <Route
+                path="profile"
+                element={
+                    <ProtectedRoute>
+                        <Profile />
+                    </ProtectedRoute>
+                }
+            />
+            <Route path="contact-us" element={<ContactUs />} />
+            <Route path="about-us" element={<AboutUs />} />
+            <Route path="privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="terms-of-service" element={<TermsOfService />} />
+            <Route path="*" element={<Page404 />} />
+        </Route>
+    )
+);
 
 function App() {
-  return (
-      <AuthContextProvider>
-        <RouterProvider router={router} />
-      </AuthContextProvider>
-  )
+    return (
+        <AuthContextProvider>
+            <RouterProvider router={router} />
+        </AuthContextProvider>
+    );
 }
 
-export default App
+export default App;
---
+++ src/constants/mocks/cardMock.js
@@ -0,0 +1,98 @@
+export const cardsObj = {
+    races: [
+        {
+            id: 0,
+            title: 'Card Race Title',
+            description: 'Card Race Description',
+            type: 'races',
+        },
+        {
+            id: 1,
+            title: 'Card Race Title 1 ',
+            description: 'Card Race Description 1',
+            type: 'races',
+        },
+        {
+            id: 2,
+            title: 'Card Race Title 2 ',
+            description: 'Card Race Description 2',
+            type: 'races',
+        },
+        {
+            id: 3,
+            title: 'Card Race Title 3 ',
+            description: 'Card Race Description 3',
+            type: 'races',
+        },
+        {
+            id: 4,
+            title: 'Card Race Title 4 ',
+            description: 'Card Race Description 4',
+            type: 'races',
+        },
+        {
+            id: 5,
+            title: 'Card Race Title 5 ',
+            description: 'Card Race Description 5',
+            type: 'races',
+        },
+        {
+            id: 6,
+            title: 'Card Race Title 6 ',
+            description: 'Card Race Description 6',
+            type: 'races',
+        },
+    ],
+    leagues: [
+        {
+            id: 0,
+            title: 'Card League Title',
+            description: 'Card League Description',
+            type: 'leagues',
+        },
+        {
+            id: 1,
+            title: 'Card League Title 1',
+            description: 'Card League Description 1',
+            type: 'leagues',
+        },
+        {
+            id: 2,
+            title: 'Card League Title 2',
+            description: 'Card League Description 2',
+            type: 'leagues',
+        },
+        {
+            id: 3,
+            title: 'Card League Title 3',
+            description: 'Card League Description 3',
+            type: 'leagues',
+        },
+        {
+            id: 4,
+            title: 'Card League Title 4',
+            description: 'Card League Description 4',
+            type: 'leagues',
+        },
+    ],
+    lives: [
+        {
+            id: 0,
+            embed: '<iframe width="560" height="315" src="https://www.youtube.com/embed/QsyPbSON09c?si=uJGZ6Sy5Mw1nVMCh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+            url: 'https://twitch.tv/gaules',
+            type: 'lives',
+        },
+        {
+            id: 1,
+            embed: '<iframe width="560" height="315" src="https://www.youtube.com/embed/ft18dshHRTc?si=xyBGtFuXf4rpbR2M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+            url: 'https://www.youtube.com/live/ft18dshHRTc?si=3-Xg-JdLiGtHVY9G',
+            type: 'lives',
+        },
+        {
+            id: 2,
+            embed: '<iframe width="560" height="315" src="https://www.youtube.com/embed/K_cm7J8C-DI?si=-3LXhqmR2a8ELLEe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+            url: 'https://youtu.be/K_cm7J8C-DI?si=-3LXhqmR2a8ELLEe',
+            type: 'lives',
+        },
+    ],
+};
