81414851e44b4457ee3767b15a03d3119e2ffadc
Vinicius-Medeiros <viniciusaomedeiros@gmail.com>
2023-10-13 17:42:03 -0300

    Solving youtube player error

--- src/Components/Card/CardPreview.jsx
+++ src/Components/Card/CardPreview.jsx
@@ -1,9 +1,31 @@
 import Embed from 'react-embed';
+import ReactPlayer from 'react-player';
 
 const CardPreview = ({ obj }) => {
+    const { url } = obj;
+
+    const checkItYouTube = str => {
+        if (str.includes('youtube') || str.includes('youtu.be')) {
+            return true;
+        } else {
+            false;
+        }
+    };
+
     return (
         <div className="flex flex-col border border-gray p-3 rounded-md max-w-md min-h-[300px] justify-start bg-cover">
-            <Embed url={obj.url} />
+            {checkItYouTube ? (
+                <ReactPlayer
+                    url={url}
+                    config={{
+                        youtube: {
+                            playerVars: { origin: 'https://www.youtube.com' },
+                        },
+                    }}
+                />
+            ) : (
+                <Embed url={url} />
+            )}
         </div>
     );
 };
