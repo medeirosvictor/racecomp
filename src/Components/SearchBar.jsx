c4693b9070354ada61f91ff1bbea44e7c252afcd
Victor Medeiros <victoramedeiros@gmail.com>
2023-12-22 23:52:48 -0500

    Improved search styles and component

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -5,6 +5,7 @@ import { searchForMatchesFirebase } from '../firebase';
 import { tempState$ } from '../utils/legendState';
 import { Memo, useSelector } from "@legendapp/state/react";
 import { observable } from '@legendapp/state';
+import SearchResult from './SearchResult';
 
 
 export const SearchBar = ({iconStyles}) => {
@@ -23,7 +24,6 @@ export const SearchBar = ({iconStyles}) => {
             if (u.length >= 1 || l.length >= 1) {
                 usersFound$.set(u);
                 leaguesFound$.set(l);
-                console.log(searchString$.get())
                 console.log('Search Results: ', usersFound$.get(), leaguesFound$.get());
             }
         }
@@ -39,31 +39,31 @@ export const SearchBar = ({iconStyles}) => {
 
     return (
         <div className='md:min-w-[80%]'>
-            <div className="flex bg-slate-200 w-full p-2 items-center mx-3 rounded-full">
-                <IconContext.Provider value={iconStyles}>
-                    <HiMagnifyingGlass />
-                </IconContext.Provider>
-                <input
-                    className="overflow-ellipsis px-1 bg-transparent outline-none w-full"
-                    type="text"
-                    placeholder="Search for leagues, pilots and more!"
-                    onChange={handleSearchBarInput}
-                    onKeyDown={handleKeyDown}
-                />
-            </div>
-
-            {/* Implementar um spinner enquanto digitando */}
-
-            <div className={`bg-slate-200 border-2 border-slate-800 p-1 `}>
+            <div className="flex flex-wrap bg-slate-200 w-full p-2 items-center mx-3 rounded-full relative">
+                <div className='flex w-full justify-start'>
+                    <input
+                        className="overflow-ellipsis px-1 bg-transparent outline-none w-full"
+                        type="text"
+                        placeholder="Search for leagues, pilots and more!"
+                        onChange={handleSearchBarInput}
+                        onKeyDown={handleKeyDown}
+                    />
+                </div>
+                <div className={`bg-white min-h-[150px] w-full p-3 absolute top-[40px] rounded-md z-10 shadow-md ${searchString$.get().length > 1 ? '' : 'hidden'}`}>
                     <div className="">
                         {usersFound.map((user) => (
-                                        <div key={user.displayName}>{user.displayName}</div>
+                            <SearchResult result={user} key={user.displayName}>{user.displayName}</SearchResult>
                         ))}
                         {leaguesFound.map((league) => (
-                            <div key={league.displayName}>{league.displayName}</div>
+                            <SearchResult result={league} key={league.displayName}>{league.displayName}</SearchResult>
                         ))}
                     </div>
+                </div>
             </div>
+
+            {/* MAYBE: Implementar um spinner enquanto digitando talvez */}
+
+            
         </div>
     )
 }
